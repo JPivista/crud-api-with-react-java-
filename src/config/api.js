86718fd00b4dff71051aacf93c958953a50f0afc
JP Jayaprathap <jp@JPs-MacBook-Pro.local>
2026-06-18 19:48:39 +0530

    api is updated

--- src/config/api.js
+++ src/config/api.js
@@ -7,7 +7,7 @@ export const API_BASE_URL = import.meta.env.DEV
 
 export const ENDPOINTS = {
   createUser: API_BASE_URL,
-  allUsers: `http://192.168.1.34:8080/api/crud/users/all_users`,
+  allUsers: `https://crud-springboot-production-ccb9.up.railway.app/api/crud/users/all_users`,
   deleteUser: (id) => `${API_BASE_URL}/delete/${id}`,
   updateUser: (id) => `${API_BASE_URL}/update/${id}`,
 }
